e76f66ad426fdbcd7f036aff0d106edb5a3cd51e
Melanie Burger <melanie.brgr@gmail.com>
2016-09-04 12:16:55 -0400

    let-const-blockScoping

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
+// ES6 test modules:
+// './let-const-blockScoping.js'
+
 var es6 = require('./let-const-blockScoping.js');
 
-let msg = "HELLO WORLD??!!";
 console.log(es6);
--- js/let-const-blockScoping.js
+++ js/let-const-blockScoping.js
@@ -1,17 +1,28 @@
 //let, const and block scoping
 
-//1
-let arr = [];
+//1 let and const are block-scoped
+let arr1 = [];
 for (var i=0; i<2; i++) {
-	arr.push( function() {return i} );
+	arr1.push( function() {return i} );
 }
 
-//2
-console.log(hoisted);
-console.log(notHoisted);
-var hoisted = "var is hoisted";
-let notHoisted = "let is not hoisted, but babel will change it to var";
+let arr2 = [];
+for (let i=0; i<2; i++) {
+	arr2.push( function() {return i} );
+}
+
+//3 const cannot be reassigned
+const PASSWORD = "password";
+// PASSWORD = "new password";
 
 module.exports = {
-	"1": arr[0]()
+	"1.1": arr1[0](), //2
+	"1.2": arr2[0](), //0
+	"2.1": hoisted, //undefined
+	// "2.2": notHoisted //ReferenceError
+	"3": PASSWORD //typeError
 }
+
+//2 let and const are not hoisted, unlike var
+var hoisted = "var is hoisted";
+let notHoisted = "let is not hoisted, but note that babel will change it to var";
